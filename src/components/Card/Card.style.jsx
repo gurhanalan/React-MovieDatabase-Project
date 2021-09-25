6c7646964424fe3b88eccd946d90c628b0e15264
Gurhan <83542792+gurhanalan@users.noreply.github.com>
2021-09-25 17:11:32 +0300

    Add card style

--- src/components/Card/Card.style.jsx
+++ src/components/Card/Card.style.jsx
@@ -4,13 +4,27 @@ export const StyledCardWrapper = styled.div`
     width: 30rem;
     height: 40rem;
     /* border: solid 1px black; */
-    box-shadow: 0 1rem 3rem rgba(0, 0, 0, 0.2);
+    box-shadow: 0 0.5rem 2rem rgba(0, 0, 0, 0.2);
     display: flex;
     align-items: center;
-    justify-content: center;
+    /* justify-content: center; */
+    flex-direction: column;
+    padding: 0.5rem;
+    transition: 0.3s;
+
+    &:hover {
+        transform: scale(1.05);
+    }
 `;
 
 export const StyledCardText = styled.p`
     color: red;
-    font-size: 2rem;
+    font-size: 1.8rem;
+    text-align: center;
+`;
+
+export const StyledCardImage = styled.img`
+    height: 85%;
+    width: auto;
+    object-fit: cover;
 `;
