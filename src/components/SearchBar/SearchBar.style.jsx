6a4dbfc6ea0d6524856b47c744bd93ffce2492ed
Gurhan <83542792+gurhanalan@users.noreply.github.com>
2021-09-26 17:13:19 +0300

    Add Searchbar style

---
+++ src/components/SearchBar/SearchBar.style.jsx
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+export const StyledSearchBarInput = styled.input`
+    color: red;
+    padding: 0.5rem;
+    font-size: 2rem;
+    width: 20rem;
+`;
+
+export const StyledSearchBarButton = styled.button`
+    background-color: red;
+    font-size: 2rem;
+    color: black;
+    font-weight: bolder;
+    padding: 0.5rem 1rem;
+    border: none;
+`;
+
+export const StyledSearchBarWrapper = styled.div`
+    display: flex;
+    justify-content: center;
+    gap: 2rem;
+    padding: 3rem 0;
+`;
